0e5c999ad9545d1cdaffc95f35a1211a5c2cd562
Saurabh Banka <saurabh.banka@gmail.com>
2020-03-16 18:25:42 +0530

    Added a navigation bar

--- views/view.go
+++ views/view.go
@@ -4,8 +4,9 @@ import "html/template"
 
 func NewView(layout string, files ...string) *View {
 	files = append(files,
-		"views/layouts/footer.gohtml",
 		"views/layouts/bootstrap.gohtml",
+		"views/layouts/navbar.gohtml",
+		"views/layouts/footer.gohtml",
 	)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
