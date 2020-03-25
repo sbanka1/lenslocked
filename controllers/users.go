68a785313ce097dd5afe289e1b8270e4f690d5f2
Saurabh Banka <saurabh.banka@gmail.com>
2020-03-25 18:35:24 +0530

    Created users controller and updated main.go to use it instead of the signup function

---
+++ controllers/users.go
@@ -0,0 +1,23 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/sbanka1/lenslocked/views"
+)
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
+
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/sbanka1/lenslocked/controllers"
 	"github.com/sbanka1/lenslocked/views"
 )
 
 var (
 	homeView    *views.View
 	contactView *views.View
-	signupView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -24,11 +24,6 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
-
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>We could not find the page you were looking for :(</h1><p>Please email us if you keep being sent to an invalid page</p>")
@@ -37,12 +32,12 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New)
 	r.NotFoundHandler = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 }
