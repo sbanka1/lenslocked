9cfb54d33e93d6158519e17fa5f30d993f1d86cb
Saurabh Banka <saurabh.banka@gmail.com>
2020-03-27 20:50:10 +0530

    DRYed up the form parsing code

--- controllers/users.go
+++ controllers/users.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/schema"
 	"github.com/sbanka1/lenslocked/views"
 )
 
@@ -38,13 +37,8 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		panic(err)
-	}
-
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
 	fmt.Fprintln(w, form)
