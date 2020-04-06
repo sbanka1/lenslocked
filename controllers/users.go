864d316f3ba072a106158099315d874199f14639
Saurabh Banka <saurabh.banka@gmail.com>
2020-04-07 01:29:16 +0530

    Connected the users controller to the users model

--- controllers/users.go
+++ controllers/users.go
@@ -4,20 +4,24 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/sbanka1/lenslocked/models"
 	"github.com/sbanka1/lenslocked/views"
 )
 
-func NewUsers() *Users {
+func NewUsers(us *models.UserService) *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "users/new"),
+		us:      us,
 	}
 }
 
 type Users struct {
 	NewView *views.View
+	us      *models.UserService
 }
 
 type SignupForm struct {
+	Name     string `schema:"name"`
 	Email    string `schema:"email"`
 	Password string `schema:"password"`
 }
@@ -40,6 +44,13 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var form SignupForm
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
+	}
+	user := models.User{
+		Name:  form.Name,
+		Email: form.Email,
+	}
+	if err := u.us.Create(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	fmt.Fprintln(w, form)
 }
