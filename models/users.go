bb8ca9670f85fe8f93ef81904bd63bf9f1b2d184
Saurabh Banka <saurabh.banka@gmail.com>
2020-04-06 22:47:34 +0530

    Finished implementing a basic UserService

--- models/users.go
+++ models/users.go
@@ -11,6 +11,10 @@ var (
 	// ErrNotFound is returned when a recource cannot be found
 	// in the database
 	ErrNotFound = errors.New("models: resource not found")
+
+	// ErrInvalidID is returned when an invalid ID is provided
+	// to a method like Delete
+	ErrInvalidID = errors.New("models: ID provided was invalid")
 )
 
 func NewUserService(connectionInfo string) (*UserService, error) {
@@ -18,8 +22,7 @@ func NewUserService(connectionInfo string) (*UserService, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	// defer db.Close()
+	db.LogMode(true)
 	return &UserService{
 		db: db,
 	}, nil
@@ -36,15 +39,54 @@ type UserService struct {
 // with more information
 func (us *UserService) ByID(id uint) (*User, error) {
 	var user User
-	err := us.db.Where("id=?", id).First(&user).Error
-	switch err {
-	case nil:
-		return &user, nil
-	case gorm.ErrRecordNotFound:
-		return nil, ErrNotFound
-	default:
-		return nil, err
+	db := us.db.Where("id=?", id)
+	err := first(db, &user)
+	return &user, err
+}
+
+// ByEmail looks up a user with the given email address and
+// returns that user.
+// If the user is found, it will return a nil error
+// If the user is not found, it will return ErrNotFound
+// If there is any other error, it will return an error
+// with more information
+func (us *UserService) ByEmail(email string) (*User, error) {
+	var user User
+	db := us.db.Where("email=?", email)
+	err := first(db, &user)
+	return &user, err
+}
+
+// first will query using the provided gorm.DB and it will
+// get the first item returned and place it into dst. If
+// nothing is found in the query, it will return ErrNotFound
+func first(db *gorm.DB, dst interface{}) error {
+	err := db.First(dst).Error
+	if err == gorm.ErrRecordNotFound {
+		return ErrNotFound
 	}
+	return err
+}
+
+// Create will create the provided user and backfill data
+// like the ID, CreatedAt, and UpdatedAt fields.
+func (us *UserService) Create(user *User) error {
+	return us.db.Create(user).Error
+}
+
+// Update will update the provided users with all of the data
+// in the provided User object.
+func (us *UserService) Update(user *User) error {
+	return us.db.Save(user).Error
+}
+
+// Delete will delete the user with the provided ID
+func (us *UserService) Delete(id uint) error {
+	if id == 0 {
+		return ErrInvalidID
+	}
+	user := User{Model: gorm.Model{ID: id}}
+	return us.db.Delete(&user).Error
 
 }
 
