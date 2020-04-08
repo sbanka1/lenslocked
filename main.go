adf1c382b2b315d08a20bc2fe506605f96ca4346
Saurabh Banka <saurabh.banka@gmail.com>
2020-04-08 23:04:49 +0530

    Created the login form

--- main.go
+++ main.go
@@ -33,6 +33,8 @@ func main() {
 	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+	r.Handle("/login", usersC.LoginView).Methods("GET")
+	r.HandleFunc("/login", usersC.Login).Methods("POST")
 	fmt.Println("Starting new server at :3000...")
 	http.ListenAndServe(":3000", r)
 }
