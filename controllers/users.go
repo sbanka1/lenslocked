61174a97538d2d80fa740c71ffa1e05b6d885d78
Saurabh Banka <saurabh.banka@gmail.com>
2020-04-19 22:04:41 +0530

    Rendered a dynamic alert

--- controllers/users.go
+++ controllers/users.go
@@ -34,7 +34,15 @@ type SignupForm struct {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	if err := u.NewView.Render(w, nil); err != nil {
+	type Alert struct {
+		Level   string
+		Message string
+	}
+	a := Alert{
+		Level:   "warning",
+		Message: "Successfully rendered a dynamic alert!",
+	}
+	if err := u.NewView.Render(w, a); err != nil {
 		panic(err)
 	}
 }
