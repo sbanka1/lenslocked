ef4d0eb9df42d382e53e5f0aec61c822f365d070
Saurabh Banka <saurabh.banka@gmail.com>
2020-04-20 17:44:58 +0530

    Implemented a PublicError interface to ensure only public error messags are shown to users

--- models/users.go
+++ models/users.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"errors"
 	"regexp"
 	"strings"
 
@@ -11,48 +10,6 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/sbanka1/lenslocked/hash"
 	"github.com/sbanka1/lenslocked/rand"
-)
-
-var (
-	// ErrNotFound is returned when a recource cannot be found
-	// in the database
-	ErrNotFound = errors.New("models: resource not found")
-
-	// ErrIDInvalid is returned when an invalid ID is provided
-	// to a method like Delete
-	ErrIDInvalid = errors.New("models: ID provided was invalid")
-
-	// ErrPasswordIncorrect is returned when an invalid password
-	// is used when attempting to authenticate a user.
-	ErrPasswordIncorrect = errors.New("models: invalid password provided")
-
-	// ErrEmailRequired is returned when an email address is
-	// not provided when creating a user
-	ErrEmailRequired = errors.New("models: email address is required")
-
-	// ErrEmailInvalid is returned when an email address provided
-	// does not match any of our requirements
-	ErrEmailInvalid = errors.New("models: email address is not valid")
-
-	// ErrEmailTaken is returned when an update or create is attempted
-	// with an email address that is already in use.
-	ErrEmailTaken = errors.New("models: email address is already taken")
-
-	// ErrPasswordRequired is returned when a create is attempted
-	// without a user password provided.
-	ErrPasswordRequired = errors.New("models: password is required")
-
-	// ErrPasswordTooShort is returned when an update or create is
-	// attempted with a user password that is less than 8 characters
-	ErrPasswordTooShort = errors.New("models: password must be at least 8 characters long")
-
-	// ErrRememberRequired is returned when a create or update
-	// is attempted without a user remember token hash
-	ErrRememberRequired = errors.New("models: remember token is required")
-
-	// ErrRememberTooShort is returned when a remember token is
-	// not at least 32 bytes
-	ErrRememberTooShort = errors.New("models: remember token must be atleast 32 bytes")
 )
 
 const userPwPepper = "lenslocked-secret-string"
