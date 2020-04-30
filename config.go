a118880777862cb2159274a37782101953f83ff4
Saurabh Banka <saurabh.banka@gmail.com>
2020-04-30 23:05:45 +0530

    Implemented code to load from a json config as well as a -prod flag to require this

--- config.go
+++ config.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 type PostgresConfig struct {
 	Host     string `json:"host"`
@@ -34,10 +38,11 @@ func DefaultPostgresConfig() PostgresConfig {
 }
 
 type Config struct {
-	Port    int    `json:"port"`
-	Env     string `json:"env"`
-	Pepper  string `json:"pepper"`
-	HMACKey string `json:"hmac_key"`
+	Port     int            `json:"port"`
+	Env      string         `json:"env"`
+	Pepper   string         `json:"pepper"`
+	HMACKey  string         `json:"hmac_key"`
+	Database PostgresConfig `json:"database"`
 }
 
 func (c Config) IsProd() bool {
@@ -46,9 +51,29 @@ func (c Config) IsProd() bool {
 
 func DefaultConfig() Config {
 	return Config{
-		Port:    3000,
-		Env:     "dev",
-		Pepper:  "lenslocked-secret-string",
-		HMACKey: "lenslocked-hmac-key",
+		Port:     3000,
+		Env:      "dev",
+		Pepper:   "lenslocked-secret-string",
+		HMACKey:  "lenslocked-hmac-key",
+		Database: DefaultPostgresConfig(),
 	}
+}
+
+func LoadConfig(configReq bool) Config {
+	f, err := os.Open(".config")
+	if err != nil {
+		if configReq {
+			panic(err)
+		}
+		fmt.Println("Using the default config...")
+		return DefaultConfig()
+	}
+	var c Config
+	dec := json.NewDecoder(f)
+	err = dec.Decode(&c)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully loaded .config")
+	return c
 }
